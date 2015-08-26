72cc4d9345bd21f3550c0d906fb3f8d1734a4f80
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-08-25 21:09:33 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.render('index.html')
+});
+
+
+
+
+app.listen(3000);
+console.log('listening on 3000')
