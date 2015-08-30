1c4d108d5dfc5ee31a9ca66980d83f3f0d4415ce
Eric Moskowitz <ericmoskowitz@Erics-MacBook-Pro.local>
2015-08-30 17:57:33 -0400

    toggle sections behavior

---
+++ public/script.js
@@ -0,0 +1,12 @@
+
+$("#eric").click(function(){
+    var active = $(".show");
+    active.toggleClass("hide show");
+    $("#landing").toggleClass("hide show");
+});
+
+$("#about").click(function(){
+    var active = $(".show");
+    active.toggleClass("hide show");
+    $("#aboutBox").toggleClass("hide show");
+});
