43c79080f5b6bcdbdda5b73aba5ed2e6f71d022c
Eric Moskowitz <moskowitz.e@gmail.com>
2019-08-01 21:45:40 -0400

    updates

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var app = express();
 app.use(cors());
 app.use(express.static('public'));
 
-app.get('/', function(req,res){
+app.get('/', function(req, res) {
     res.render('index.html')
 });
 
-app.get('/cg', function(req,res){
-    res.redirect('cg.html')
+app.get('/resume', function(req, res) {
+    res.render('assets/Eric_Moskowitz_Resume_08_2019.pdf')
 });
 
 app.listen(80);
